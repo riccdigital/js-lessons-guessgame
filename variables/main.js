5bcc5942945466bd0198337a122e8559b5ea23cd
riccdigital <eryc.mabonga@gmail.com>
2020-10-29 08:47:51 +0300

    alert corrections

--- variables/main.js
+++ variables/main.js
@@ -1,10 +1,10 @@
 const button = document.querySelector('button')
 
 button.onclick = function () {
-  let age = prompt('Enter your age: ')
+  let age = window.prompt('Enter your age: ')
   if (age >= 18) {
-    alert('You are an adult!')
+    window.alert('You are an adult!')
   } else {
-    alert ('Drink some milk!')
+    window.alert('Drink some milk!')
   }
 }
